33fe4a37aaed94b3f23081217aa110f50b18cf2c
ClaytonDewey <clay@dryadmedia.com>
2022-05-26 09:05:46 -0500

    Import and use ApprovalCard

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CommentDetail } from "./CommentDetail";
 import { faker } from "@faker-js/faker";
+import { ApprovalCard } from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                image={faker.image.avatar()}
-                author="Sam"
-                timeAgo="Today at 4:45PM"
-                comment="Nice blog post!"
-            />
-            <CommentDetail
-                image={faker.image.avatar()}
-                author="Alex"
-                timeAgo="Today at 2:00AM"
-                comment="Good point..."
-            />
-            <CommentDetail
-                image={faker.image.avatar()}
-                author="Jane"
-                timeAgo="Yesterday at 5:00PM"
-                comment="Did your parents have any children that lived?"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    image={faker.image.avatar()}
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    comment="Nice blog post!"
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    image={faker.image.avatar()}
+                    author="Alex"
+                    timeAgo="Today at 2:00AM"
+                    comment="Good point..."
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    image={faker.image.avatar()}
+                    author="Jane"
+                    timeAgo="Yesterday at 5:00PM"
+                    comment="Did your parents have any children that lived?"
+                />
+            </ApprovalCard>
         </div>
     );
 };
